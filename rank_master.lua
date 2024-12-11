b7d403cb0461afd018f9367122b2c374274164f7
Zo4a <86344622+Zo4a@users.noreply.github.com>
2024-12-11 21:20:24 +0200

    Create rank_master.lua

---
+++ rank_master.lua
@@ -0,0 +1,20 @@
+getgenv().Config = {
+    ["EquipEnchants"] = {"Coins", "Diamonds", "Treasure Hunter", "Strong Pets", "Tap Power"},
+    ["KickWhenReachGoal"] = {
+        ["Rank"] = 33,
+        ["Rebirth"] = 9,
+        ["Area"] = 234
+    },
+    ["StopAt"] = {
+        ["Rank"] = 8, 
+        ["Area"] = 234
+    },
+    
+    ["AutoOpen"] = true,
+    ["AutoBalloon"] = true, -- WANT A BAN? REPORT A BUG IN THIS SCRIPT.
+
+    ["FPSBooster"] = true, -- REPORT NO SCRIPT BUGS.
+    ["UltraFPS"] = true, -- I WILL BAN YOU IF YOU REPORT ANY SCRIPT BUGS.
+    ["Webhook"] = "", -- DO NOT REPORT ANY SCRIPT BUGS. WE KNOW ITS BUGGED.
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/177d65e916988f4b84dd21ed33a2aa91.lua"))()
